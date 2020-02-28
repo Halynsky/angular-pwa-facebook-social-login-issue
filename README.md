eb91097b125ce5dec988a0b53f128fd28eff378c
Halynsky <off.bk90@gmail.com>
2020-02-28 18:33:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-pwa-facebook-social-login-issue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-pwa-facebook-social-login-issue)
---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+declare let FB: any;
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  user: any = null;
+
+ authFacebookNative(){
+    let fbLoginOptions = {
+      scope: 'pages_messaging,pages_messaging_subscriptions,email,pages_show_list,manage_pages',
+      return_scopes: true,
+      enable_profile_selector: true
+    };
+    FB.login((response: any) => {
+      alert(JSON.stringify(response));
+      if (response.authResponse) {
+        let authResponse = response.authResponse;
+        FB.api(`/me?fields=name,email,last_name,first_name,picture`, (fbUser: any) => {
+          this.user = {};
+
+          this.user.id = fbUser.id;
+          this.user.name = fbUser.name;
+          this.user.email = fbUser.email;
+          this.user.photoUrl = 'https://graph.facebook.com/' + fbUser.id + '/picture?type=normal';
+          this.user.firstName = fbUser.first_name;
+          this.user.lastName = fbUser.last_name;
+          this.user.authToken = authResponse.accessToken;
+
+          this.user.facebook = fbUser;
+
+          alert(JSON.stringify(this.user));
+        });
+      } else {
+        // error
+      }
+    }, fbLoginOptions);
+  }
+
+
+}
