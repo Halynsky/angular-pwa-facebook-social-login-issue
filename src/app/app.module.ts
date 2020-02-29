b9b01562cc8eebdd8378899da9048ee5e8189768
Halynsky <off.bk90@gmail.com>
2020-02-29 06:40:13 +0200

    3 files modified, 3 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
